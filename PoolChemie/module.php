e2309ca3851b676ea2f3219a6c2d90c794aaa118
JohannesMaennel <johannes.maennel@outlook.com>
2026-07-04 17:27:00 +0200

    Add Create and ApplyChanges methods to PoolChemie

--- PoolChemie/module.php
+++ PoolChemie/module.php
@@ -1,3 +1,14 @@
+<?php
+
 class PoolChemie extends IPSModule
 {
+    public function Create()
+    {
+        parent::Create();
+    }
+
+    public function ApplyChanges()
+    {
+        parent::ApplyChanges();
+    }
 }
