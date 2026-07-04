bf24d76f1cc3cd2917d7b78097e5cd2516b66001
JohannesMaennel <johannes.maennel@outlook.com>
2026-07-04 17:12:56 +0200

    Add properties for MQTT and scale types
    Register properties for MQTT instance and scale types.

---
+++ PoolChemie/modul.php
@@ -0,0 +1,7 @@
+$this->RegisterPropertyInteger("MQTTInstance", 0);
+$this->RegisterPropertyInteger("ScaleCount", 2);
+
+$this->RegisterPropertyString("Scale1Type", "Chlor");
+$this->RegisterPropertyString("Scale2Type", "pH Minus");
+$this->RegisterPropertyString("Scale3Type", "Flockungsmittel");
+$this->RegisterPropertyString("Scale4Type", "Aktivsauerstoff");
