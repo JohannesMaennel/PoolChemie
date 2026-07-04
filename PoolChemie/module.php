d9e15c619033b2e3a2127ad819bf4ecbbe79d739
JohannesMaennel <johannes.maennel@outlook.com>
2026-07-04 17:24:35 +0200

    Update module.php

--- PoolChemie/module.php
+++ PoolChemie/module.php
@@ -1,7 +1,3 @@
-$this->RegisterPropertyInteger("MQTTInstance", 0);
-$this->RegisterPropertyInteger("ScaleCount", 2);
-
-$this->RegisterPropertyString("Scale1Type", "Chlor");
-$this->RegisterPropertyString("Scale2Type", "pH Minus");
-$this->RegisterPropertyString("Scale3Type", "Flockungsmittel");
-$this->RegisterPropertyString("Scale4Type", "Aktivsauerstoff");
+class PoolChemie extends IPSModule
+{
+}
