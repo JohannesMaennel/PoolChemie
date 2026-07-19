0ee6b8bb9575f28c61aa921950cc9d6e2383c3c2
JM Privat <johannes.maennel@outlook.com>
2026-07-19 22:12:10 +0200

    Updated Drucksensor.yaml with correct GPIO pins and WiFi credentials. Added test.yaml for testing purposes. Added EmporiaEnergy module files (form.json, module.json, module.php) and IPS.functions.ips.php for additional functionality.

---
+++ EmporiaEnergy/module.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+class EmporiaEnergy extends IPSModule
+{
+
+public function Create()
+{
+    parent::Create();
+
+}
+
+public function Destroy()
+{
+    parent::Destroy();
+}
+
+public function ApplyChanges()
+{
+    parent::ApplyChanges();
+}
+
+}
